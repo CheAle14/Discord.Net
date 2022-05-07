1e06ead328fa2f93f27c2d3d008bc1a3aa1d9ce9
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2022-05-07 11:30:05 +0100

    Remove check for archive duration

--- src/Discord.Net.Rest/Entities/Channels/ThreadHelper.cs
+++ src/Discord.Net.Rest/Entities/Channels/ThreadHelper.cs
@@ -12,11 +12,11 @@ namespace Discord.Rest
             ThreadArchiveDuration autoArchiveDuration = ThreadArchiveDuration.OneDay, IMessage message = null, bool? invitable = null, int? slowmode = null, RequestOptions options = null)
         {
             var features = channel.Guild.Features;
-            if (autoArchiveDuration == ThreadArchiveDuration.OneWeek && !features.HasFeature(GuildFeature.SevenDayThreadArchive))
+            /*if (autoArchiveDuration == ThreadArchiveDuration.OneWeek && !features.HasFeature(GuildFeature.SevenDayThreadArchive))
                 throw new ArgumentException($"The guild {channel.Guild.Name} does not have the SEVEN_DAY_THREAD_ARCHIVE feature!", nameof(autoArchiveDuration));
 
             if (autoArchiveDuration == ThreadArchiveDuration.ThreeDays && !features.HasFeature(GuildFeature.ThreeDayThreadArchive))
-                throw new ArgumentException($"The guild {channel.Guild.Name} does not have the THREE_DAY_THREAD_ARCHIVE feature!", nameof(autoArchiveDuration));
+                throw new ArgumentException($"The guild {channel.Guild.Name} does not have the THREE_DAY_THREAD_ARCHIVE feature!", nameof(autoArchiveDuration));*/
 
             if (type == ThreadType.PrivateThread && !features.HasFeature(GuildFeature.PrivateThreads))
                 throw new ArgumentException($"The guild {channel.Guild.Name} does not have the PRIVATE_THREADS feature!", nameof(type));
